edc36580790af42f52f118ec72892ccfd735a2ca
Mattia Ducoli <121670304+mducoli@users.noreply.github.com>
2024-11-12 10:22:02 +0100

    flake refactor

--- flake.nix
+++ flake.nix
@@ -4,45 +4,43 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixvim.url = "github:nix-community/nixvim";
-    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs = {
+    nixpkgs,
     nixvim,
-    flake-parts,
     ...
-  } @ inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
+  }: let
+    lib = nixpkgs.lib;
 
-      perSystem = {
-        pkgs,
-        system,
-        ...
-      }: let
-        nixvimLib = nixvim.lib.${system};
-        nixvim' = nixvim.legacyPackages.${system};
-        nixvimModule = {
-          inherit pkgs;
-          module = import ./config; # import the module directly
-          # You can use `extraSpecialArgs` to pass additional arguments to your module files
-        };
-        nvim = nixvim'.makeNixvimWithModule nixvimModule;
-      in {
-        checks = {
-          # Run `nix flake check .` to verify that your config is not broken
-          default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
-        };
+    systems = [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+      "aarch64-linux"
+    ];
 
-        packages = {
-          # Lets you run `nix run .` to start nixvim
-          default = nvim;
-        };
-      };
+    forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
+    pkgsFor = lib.genAttrs systems (system:
+      import nixpkgs {
+        inherit system;
+      });
+
+    nixvimModule = pkgs: {
+      inherit pkgs;
+      module = import ./config;
     };
+  in {
+    checks = forEachSystem (pkgs: let
+      nixvimLib = nixvim.lib.${pkgs.system};
+    in {
+      default = nixvimLib.check.mkTestDerivationFromNixvimModule (nixvimModule pkgs);
+    });
+
+    packages = forEachSystem (pkgs: let
+      nixvim' = nixvim.legacyPackages.${pkgs.system};
+    in {
+      default = nixvim'.makeNixvimWithModule (nixvimModule pkgs);
+    });
+  };
 }
