2ed876dbb8687c574ad2f3904860ff48b9952ded
Mattia Ducoli <121670304+mducoli@users.noreply.github.com>
2025-09-07 12:57:35 +0200

    clean flake

--- flake.nix
+++ flake.nix
@@ -9,6 +9,11 @@
     import-tree.url = "github:vic/import-tree";
   };
 
+  nixConfig = {
+    abort-on-warn = true;
+    allow-import-from-derivation = false;
+  };
+
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
@@ -19,59 +24,5 @@
         inputs.flake-parts.flakeModules.modules
         (inputs.import-tree ./modules)
       ];
-
     };
-
-  /*
-    outputs =
-      {
-        self,
-        nixpkgs,
-        systems,
-        nixvim,
-        ...
-      }:
-      let
-        inherit (self) outputs;
-        inherit (nixpkgs) lib;
-        forEachSystem = f: lib.genAttrs (import systems) (system: f pkgsFor.${system});
-        pkgsFor = lib.genAttrs (import systems) (
-          system:
-          import nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
-          }
-        );
-      in
-      {
-        checks = forEachSystem (
-          pkgs:
-          builtins.mapAttrs (
-            n: v:
-            nixvim.lib.${pkgs.system}.check.mkTestDerivationFromNvim {
-              name = n;
-              nvim = v;
-            }
-          ) outputs.packages.${pkgs.system}
-        );
-
-        packages = forEachSystem (
-          pkgs: with nixvim.legacyPackages.${pkgs.system}; rec {
-
-            minimal = makeNixvimWithModule {
-              inherit pkgs;
-              module = import ./config/global;
-            };
-
-            default = minimal.extend {
-              imports = [ ./config/full.nix ];
-            };
-          }
-        );
-
-        hydraJobs = {
-          pkgs = outputs.packages;
-        };
-      };
-  */
 }
